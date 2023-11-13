1f7d6f81c2b95b5921b2af9d148d877f85531ce1
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2023-11-13 18:52:00 +0100

    unite le page

--- sideExtra.js
+++ sideExtra.js
@@ -14,16 +14,18 @@ close.onclick = () => {
   aside.classList.add("translate");
   aside.classList.add("d-none");
   aside.classList.remove("d-block");
-  aside.classList.remove("col-4");
-  main.classList.remove("col-8");
+  aside.classList.remove("col-2");
+  main.classList.remove("col-7");
+  main.classList.add("col-9");
 };
 
 btnF.onclick = () => {
   aside.classList.remove("translate");
   aside.classList.remove("d-none");
   aside.classList.add("d-block");
-  aside.classList.add("col-4");
-  main.className = "col-8";
+  aside.classList.add("col-2");
+  main.classList.remove("col-9");
+  main.classList.add("col-7")
 };
 
 btnAdd.onclick = () => {
