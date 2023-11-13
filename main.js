8dd15def4eac4ff6a06e91eadbbcc4728dbc26b9
Rafael98-stack <rafaelcasapaojr@gmail.com>
2023-11-13 13:25:11 +0100

    FirstCommit

---
+++ main.js
@@ -0,0 +1,90 @@
+// BOTTONE "Visualizza tutto"
+
+const buttonSection2 = document.getElementById("buttonSection2")
+const cards2 = document.getElementById("cards2")
+cards2.setAttribute("id","cards")
+buttonSection2.onclick = function () {
+  cards2.innerHTML = `
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  <div id="col-20">
+    <div class="card bg-secondary text-white">
+        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+        </div>
+      </div>
+    </div>
+  `
+ 
+}
