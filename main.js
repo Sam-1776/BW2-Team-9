af687f0ce758c864a1a5938b9c6a2815ea98d91f
Rafael98-stack <rafaelcasapaojr@gmail.com>
2023-11-13 16:35:26 +0100

    Correcting

--- main.js
+++ main.js
@@ -1,90 +1,59 @@
 // BOTTONE "Visualizza tutto"
+fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=tyga", {
+    headers: {
+      'X-RapidAPI-Key': '971f3683c1mshd0d96937de5880fp110e78jsn1242e8b2381c',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+})
+.then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    
+  })
+  .catch((error) => console.error("Errore durante la richiesta:", error));
 
 const buttonSection2 = document.getElementById("buttonSection2")
 const cards2 = document.getElementById("cards2")
 cards2.setAttribute("id","cards")
+
+
 buttonSection2.onclick = function () {
-  cards2.innerHTML = `
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  <div id="col-20">
-    <div class="card bg-secondary text-white">
-        <img src="./assets/imgs/search/image-8.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
-        </div>
-      </div>
-    </div>
-  `
- 
+    
+    cardsSongsLoaded()
 }
+const cardsSongsLoaded = () => {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=tyga", {
+      headers: {
+    	'X-RapidAPI-Key': '971f3683c1mshd0d96937de5880fp110e78jsn1242e8b2381c',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+})
+.then(resp => {
+console.log(resp)
+ return  resp.json() }) 
+        .then(song => {
+            cardsSongs(song)
+            console.log(song);
+           
+        })
+        .catch(err =>
+            console.error(err.message)
+        )
+        
+        function cardsSongs (song) {
+            cards2.innerHTML = "";
+
+                song.forEach(song => {
+                cards2.innerHTML = `
+                <div id="col-20">
+                <div class="card bg-secondary text-white">
+                <img src="${song.picture_medium}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, quae.</p>
+                </div>
+                </div>
+                </div>
+                `
+        })
+        }
+    }
