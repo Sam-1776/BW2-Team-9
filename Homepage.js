91d802e723a94eee05d63cc3cde6a2a3ce2841ad
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2023-11-15 01:05:24 +0100

    add result reserch

--- Homepage.js
+++ Homepage.js
@@ -41,20 +41,102 @@ barInput.onchange = () => {
     .catch((err) => console.log(err));
 };
 
+const container = document.querySelector("#searchQ")
 const Artist = (x) => {
-  const div = document.querySelector("#Ricerche");
+  newSection.classList.add("d-none");
+  const row = document.createElement("div");
+  row.className = "col-5"
   const col = document.createElement("div");
-  col.className = "col-6 rounded";
+  col.className = "rounded bg-dark";
+  col.style = "height: 216px"
+  const div = document.createElement("div")
+  div.className = "m-3"
   const a = document.createElement("a");
   const img = document.createElement("img");
-  img.className = "rounded-circle overflow-hidden img-fluid";
+  img.className = "rounded-circle overflow-hidden img-fluid mb-3 mt-2";
+  img.style = "width: 80px; height:80px"
   const h3 = document.createElement("h3");
+  h3.className = "fw-bold"
+  const divT = document.createElement("div");
+  /* divT.style = "width: 36px; heigth: 36px" */
+  const h4 = document.createElement("h4")
+  h4.innerText = "Risultato più rilevante"
+  const span = document.createElement("span")
+  span.className = "badge text-ligth rounded-pill"
+  span.style = "background-color: #121212"
 
   img.src = x.data[1].artist.picture_small;
   h3.innerText = x.data[1].artist.name;
+  span.innerHTML = x.data[1].artist.type;
+
 
   a.appendChild(img);
-  col.appendChild(a);
-  col.appendChild(h3);
-  div.appendChild(col);
+  div.appendChild(a);
+  div.appendChild(h3);
+  div.appendChild(span);
+  col.appendChild(div)
+  divT.appendChild(h4)
+  row.appendChild(divT)
+  row.appendChild(col);
+  container.appendChild(row)
 };
+
+
+const Brani = (x) =>{
+  const row = document.createElement("div");
+  row.className = "col-6"
+  const divT = document.createElement("div");
+  divT.className = "mb-3"
+  const h4 = document.createElement("h4")
+  h4.innerText = "Brani"
+  divT.appendChild(h4)
+  row.appendChild(divT)
+  for (let i = 0; i < 4; i++) {
+    const j = Math.floor(Math.random() * 25)
+    console.log(j);
+    const col = document.createElement("div");
+    col.className = " d-flex justify-content-between align-items-center rounded mb-2";
+    const div = document.createElement("div")
+    div.className = "d-flex"
+    const divI = document.createElement("div")
+    const divR = document.createElement("div")
+    const img = document.createElement("img");
+    img.src = x.data[j].album.cover_small
+    img.className = "rounded me-2"
+    img.style = "width: 46px; height:46px"
+    const a = document.createElement("a");
+    const an = document.createElement("a");
+    const h5 = document.createElement("h5")
+    h5.innerHTML = x.data[j].title
+    h5.className = "m-0"
+    const p = document.createElement("p")
+    p.className = "m-0"
+    p.innerHTML =  x.data[j].duration
+    const span = document.createElement("span")
+    span.innerHTML = x.data[j].artist.name
+
+
+    div.appendChild(img)
+    a.appendChild(h5)
+    an.appendChild(h5)
+    divI.appendChild(a)
+    divI.appendChild(an)
+    if (x.data[j].explicit_lyrics === true) {
+      const i = document.createElement("i")
+      i.className = "bi bi-explicit-fill d-inline-block me-2"
+      divI.appendChild(i);
+    }
+    divI.appendChild(span)
+    div.appendChild(divI)
+    
+    
+    col.appendChild(div)
+    divR.appendChild(p)
+    col.appendChild(divR)
+    row.appendChild(col);
+    
+  };
+  
+  
+  container.appendChild(row)
+}
